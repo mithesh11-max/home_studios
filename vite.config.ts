30bf4cb8f6025ed2ac9751a7bc466b38a1acae45
LDHANUSHRAJ <indesignluxuryfabrics@gmail.com>
2026-09-08 00:01:00 +0530

    auto: update vite.config.ts [08/09/2026, 00:01:00]

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
+  optimizeDeps: {
+    include: ["three", "@react-three/fiber"],
+  },
   build: {
     target: "es2022",
     rollupOptions: {
