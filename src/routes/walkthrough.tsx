95aa6b3d016997997213289d63e25d08931d12ba
LDHANUSHRAJ <indesignluxuryfabrics@gmail.com>
2026-09-07 17:30:35 +0530

    auto: update walkthrough.tsx [07/09/2026, 17:30:35]

--- src/routes/walkthrough.tsx
+++ src/routes/walkthrough.tsx
@@ -5,6 +5,7 @@ import { SiteHeader } from "@/components/site/SiteHeader";
 import { SiteFooter } from "@/components/site/SiteFooter";
 import { FinalCTA } from "@/components/site/FinalCTA";
 import { FadeUp } from "@/components/site/TextReveal";
+import { InteractiveFloorPlan } from "@/components/site/InteractiveFloorPlan";
 import { CONTACT } from "@/lib/site-data";
 
 import walkthroughFurnitureImg from "@/assets/walkthrough-furniture.jpg";
