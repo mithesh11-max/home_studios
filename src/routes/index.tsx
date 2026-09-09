dcc97732a1c82d0880c31620ddd38270eb3a3b59
LDHANUSHRAJ <indesignluxuryfabrics@gmail.com>
2026-09-09 09:56:23 +0530

    auto: update index.tsx [09/09/2026, 09:56:23]

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -14,7 +14,6 @@ import { FadeUp } from "@/components/site/TextReveal";
 import { SequenceHero } from "@/components/site/SequenceHero";
 import { SpatialProjectShowcase } from "@/components/site/SpatialProjectShowcase";
 import { ArchitecturalMeasurement } from "@/components/site/ArchitecturalMeasurement";
-import { PlanSpaceExperience } from "@/components/site/PlanSpaceExperience";
 import { useAppointment } from "@/lib/appointment-context";
 import { useSmoothScroll } from "@/lib/lenis-context";
 import { HOME_FAQS, CONTACT } from "@/lib/site-data";
